bdd06ace58263e20c881b19ac50047320f55b9de
AlvaroJorge <alv.jor96@gmail.com>
2022-02-21 19:39:26 -0300

    Agregados iconos y fondo

--- index.js
+++ index.js
@@ -1,38 +1,95 @@
+//KEY PERSONAL
 const myApiKey = '85812df68546b272fcaa70610402a638';
 
+//FETCH DE API
 const ubicacion = posicion => {
     const {latitude, longitude} = posicion.coords;
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=es&appid=${myApiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=es&appid=${myApiKey}`)
         .then(response => response.json())
-        .then(datos => datosDeClima(datos));
+        .then(datos => datosDeClima(datos))
+        .catch(error => console.log(error))
 }
 
+//OBJETO CON LOS DATOS A UTILIZAR
 const datosDeClima = datos => {
     const climaInfo = {
         region: datos.name,
         pais: datos.sys.country,
-        temperatura: `${datos.main.temp} °C`,
-        clima: `Clima: ${datos.weather[0].description}`,
+        temperatura: `Temperatura: ${datos.main.temp} °C`,
+        clima: `Clima: ${datos.weather[0].description}`.toUpperCase(),
         humedad: `Humedad: ${datos.main.humidity}%`,
         presion: `Presion: ${datos.main.pressure} hPa`,
         viento: `Viento: ${datos.wind.speed} m/s`,
-        fecha: 'Fecha: ' + fechaActual(),
+        fecha: 'Día: ' + fechaActual(),
     }
 
     Object.keys(climaInfo).forEach(key => {
         modificarTexto(key, climaInfo[key]);
     });
+
+    //MODIFICACION DE ICONOS
+    const iconos = () => {
+        let iconoClima = document.getElementById('iconoAnimado');
+        let letras = document.querySelector('.fuente');
+        switch (datos.weather[0].main) {
+            case 'Thunderstorm':
+                iconoClima.src='animated/thunder.svg';
+                document.body.style.backgroundImage = 'url(background/Storm-Skies.jpg)';
+                letras.style.color = '#fff';
+                break;
+            case 'Drizzle':
+                iconoClima.src='animated/rain-4.svg';
+                document.body.style.backgroundImage = 'url(background/Drizzle-Skies.jpg)';
+                letras.style.color = '#000';
+                break;
+            case 'Rain':
+                iconoClima.src='animated/rain-7.svg';
+                document.body.style.backgroundImage = 'url(background/Rain-Skies.jpg)';
+                letras.style.color = '#fff';
+                break;
+            case 'Snow':
+                iconoClima.src='animated/snowy-6.svg';
+                document.body.style.backgroundImage = 'url(background/Snow-Skies.jpg)';
+                letras.style.color = '#000';
+                break;
+            case 'Clear':
+                iconoClima.src='animated/day.svg';
+                document.body.style.backgroundImage = 'url(background/Sunny-Skies.jpg)';
+                letras.style.color = '#000';
+                break;
+            case 'Clouds':
+                iconoClima.src='animated/cloudy.svg';
+                document.body.style.backgroundImage = 'url(background/Cloud-Skies.jpg)';
+                letras.style.color = '#000';
+                break;
+        }
+    }
+    //LLAMADA DE FUNCIONES
+    iconos();
+    animacionCarga();
 }
 
+//MODIFICAR TEXTO CON KEYS
 const modificarTexto = (elemento, texto) => {
     document.getElementById(elemento).textContent = texto;
 }
 
+//OBTENER FECHA ACTUAL
 const fechaActual = () => {
     let fechaAct = new Date();
     return `${fechaAct.getDate()}/${( '0' + (fechaAct.getMonth() + 1)).slice(-2)}/${fechaAct.getFullYear()}`;
 }
 
+//PERMITIR LOCALIZACION
 const carga = () => {
     navigator.geolocation.getCurrentPosition(ubicacion);
+}
+
+//TERMINAR ANIMACION CUANDO FINALIZE LA CARGA DE DATOS
+const animacionCarga = () => {
+    let loader = document.getElementById('loader');
+    let contenedor = document.getElementById('container');
+
+    loader.style.display = 'none';
+    contenedor.style.display = 'grid';
 }
